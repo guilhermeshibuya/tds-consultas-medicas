9fab3e6a01e1a606f0de603a44f46e1e6ec9ed89
Jair José Sequeira <jsequeira@alunos.utfpr.edu.br>
2023-09-23 23:33:41 -0300

    Add: consuta e paciente

---
+++ ConsultasMedicas/ConsultasMedicas.API/Controllers/Consulta.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Healthy.Domain.Enums;
+
+
+namespace Healthy.Domain.Entities;
+
+public class ConsultaModel 
+{
+    [Display(Name = "Código")] public int Id { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "Data e hora da consulta")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
+    [DataType(DataType.DateTime, ErrorMessage = "Data inválida")]
+  s
+    public DateTime Data { get; set; }
+
+    [Display(Name = "Criado em")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
+    [DataType(DataType.DateTime, ErrorMessage = "Data inválida")]
+    public DateTime Criacao { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "Médico")]
+    public int MedicoId { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "Paciente")]
+    public int PacientetId { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "Descrição")]
+    [StringLength(500, MinimumLength = 0, ErrorMessage = "Descriçao muito longa")]
+    public string Descricao { get; set; }
+
+
+    [Display(Name = "Médico")] public MedicoModel Medico { get; set; }
+
+    [Display(Name = "Paciente")] public PacienteModel Paciente { get; set; }
+
+    [Display(Name = "Valor da consulta")]
+    [DisplayFormat(DataFormatString = "{0:C2}")]
+    public decimal Invoice { get; set; }
+}
---
+++ ConsultasMedicas/ConsultasMedicas.API/Controllers/PacienteModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Healthy.Domain.Entities;
+
+public class PacienteModel 
+{
+    [Required(ErrorMessage = "CPF é obrigatório")]
+    [Display(Name = "CPF")]
+    public string CPF { get; set; }
+    
+    public virtual ICollection<ConsultaModel> Appointments { get; set; }
+    [Required(ErrorMessage = "Informe o nome")]
+    [Display(Name = "Nome")]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Nome informado e invalido")]
+    public string PrimeiroNome { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "Sobrenome")]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "Nome informado e invalido")]
+    public string Sobrenome { get; set; }
+
+    [NotMapped]
+    [Display(Name = "Nome completo")]
+    public string FullName => PrimeiroNome + " " + Sobrenome;
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "Data de nascimento")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    [DataType(DataType.Date, ErrorMessage = "Data inválida")]
+    public DateTime Aniversario { get; set; }
+
+    [NotMapped][Display(Name = "Idade")] public int Age => (int)((DateTime.Now - Aniversario).TotalDays / 365.25);
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "E-mail")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [Display(Name = "Telefone")]
+    [Phone(ErrorMessage = "Telefone inválido")]
+    public string Telefone { get; set; }
+
+    public PacienteModel()
+    {
+        ConsultaModel = new HashSet<ConsultaModel>();
+    }
+}
