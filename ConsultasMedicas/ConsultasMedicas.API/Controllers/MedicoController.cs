b1f631a7fc828a94d832ffa3920d645f3b9b7210
Guilherme Shibuya <gyudishibuya@gmail.com>
2023-09-24 00:40:06 -0300

    Add: medico controller, metodo get

---
+++ ConsultasMedicas/ConsultasMedicas.API/Controllers/MedicoController.cs
@@ -0,0 +1,23 @@
+﻿using ConsultasMedicas.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultasMedicas.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicoController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(
+            [FromServices] AppDbContext context
+        )
+        {
+            var medico = await context.Medicos.ToListAsync();
+
+            if (medico == null) return NotFound();
+
+            return Ok(medico);
+        }
+    }
+}
