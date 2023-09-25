6be0c793529082f117f7de04678b0912b7b3a175
Jair José Sequeira <jsequeira@alunos.utfpr.edu.br>
2023-09-24 21:40:23 -0300

    Feat: Foi criado rotas para o Especialidades

--- Consultas/Consultas.API/Controllers/EspecialidadeController.cs
+++ Consultas/Consultas.API/Controllers/EspecialidadeController.cs
@@ -37,5 +37,90 @@ namespace Consultas.API.Controllers
 
             return Created($"/{especialidade.Id}", especialidade);
         }
+
+        [HttpGet("{id:int}/")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context
+)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var especialidade = await context.Especialidades.Include(e => e.Medicos)
+                .FirstOrDefaultAsync(e => e.Id == id);
+            
+            return Ok(especialidade);
+        }
+
+        [HttpDelete("{id:int}/")]
+        public async Task<IActionResult> DeleteByIdAsync(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context
+        )
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var especialidade = await context.Especialidades.FindAsync(id);
+
+            if (especialidade == null)
+            {
+                return NotFound();
+            }
+
+            context.Especialidades.Remove(especialidade);
+            await context.SaveChangesAsync();
+
+            return NoContent(); // Indica que a deleção foi bem-sucedida e não há conteúdo para retornar
+        }
+
+        [HttpPut("{id:int}/")]
+        public async Task<IActionResult> UpdateByIdAsync(
+            [FromRoute] int id,
+            [FromBody] EspecialidadeModel updatedEspecialidade,
+            [FromServices] AppDbContext context
+        )
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var especialidade = await context.Especialidades.FindAsync(id);
+
+            if (especialidade == null)
+            {
+                return NotFound();
+            }
+
+            // Atualizar os dados da especialidade com base nos dados fornecidos
+            especialidade.Nome = updatedEspecialidade.Nome;  // Substitua com os campos apropriados
+
+            // Marcar a entidade como modificada e salvar as alterações
+            context.Entry(especialidade).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!context.Especialidades.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent(); // Indica que a atualização foi bem-sucedida e não há conteúdo para retornar
+        }
     }
 }
